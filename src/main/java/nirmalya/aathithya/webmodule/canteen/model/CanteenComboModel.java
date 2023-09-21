1ae9d9d81bd012cf549b9d58827b611a2c86478f
BUlet kumar <bulet1223@gmail.com>
2023-09-21 10:42:48 +0530

    web

---
+++ src/main/java/nirmalya/aathithya/webmodule/canteen/model/CanteenComboModel.java
@@ -0,0 +1,10 @@
+package nirmalya.aathithya.webmodule.canteen.model;
+
+public class CanteenComboModel {
+	
+	private String itemId;
+	/*
+	 * private String itemId; private String itemId;
+	 * 
+	 */
+}
